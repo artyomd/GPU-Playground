289ede511c85231b413df3b7a6402e4411a7efba
Artyom Dangizyan <artyom.dangizyan@gmail.com>
2020-10-04 22:56:36 +0400

    update vendor libs add directfb support

--- cmake/FindXCB.cmake
+++ cmake/FindXCB.cmake
@@ -24,20 +24,20 @@ foreach(comp ${XCB_FIND_COMPONENTS})
     pkg_check_modules(PC_${comp} QUIET ${comp})
 
     find_path(${compname}_INCLUDE_DIR NAMES ${headername}
-        HINTS
-        ${PC_${comp}_INCLUDEDIR}
-        ${PC_${comp}_INCLUDE_DIRS}
-        )
+            HINTS
+            ${PC_${comp}_INCLUDEDIR}
+            ${PC_${comp}_INCLUDE_DIRS}
+            )
 
     find_library(${compname}_LIBRARY NAMES ${libname}
-        HINTS
-        ${PC_${comp}_LIBDIR}
-        ${PC_${comp}_LIBRARY_DIRS}
-        )
+            HINTS
+            ${PC_${comp}_LIBDIR}
+            ${PC_${comp}_LIBRARY_DIRS}
+            )
 
     find_package_handle_standard_args(${comp}
-        FOUND_VAR ${comp}_FOUND
-        REQUIRED_VARS ${compname}_INCLUDE_DIR ${compname}_LIBRARY)
+            FOUND_VAR ${comp}_FOUND
+            REQUIRED_VARS ${compname}_INCLUDE_DIR ${compname}_LIBRARY)
     mark_as_advanced(${compname}_INCLUDE_DIR ${compname}_LIBRARY)
 
     list(APPEND XCB_INCLUDE_DIRS ${${compname}_INCLUDE_DIR})
